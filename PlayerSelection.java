895e2b2153d1fc3e2d06ca367bbec8e7879bc300
Maneth-M2 <86797953+Maneth-M2@users.noreply.github.com>
2021-07-07 15:54:42 +0530

    Update PlayerSelection.java

--- PlayerSelection.java
+++ PlayerSelection.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PlayerSelection {
     public static void main(String[] args) {
@@ -13,12 +14,30 @@ public class PlayerSelection {
         playerList.add(newPlayer3);
         playerList.add(newPlayer4);
 
-        for(int i = 0 ;i< playerList.size(); i++) {
-            System.out.println("Player Name : " + playerList.get(i).getPlayerName());
-            System.out.println("Player Age : " + playerList.get(i).getPlayerAge());
-            System.out.println("Player Type : " + playerList.get(i).getPlayerType());
-            System.out.println("Player Statistics : " + playerList.get(i).getPlayerStatistics());
+        while (true) {
+            Scanner myScnr = new Scanner(System.in);
+            System.out.println("Please Select an Option : \nA : Add a Player\nB : Display two best batsman\nC : Display two best bowlers\nD : Display two best keepers\nV : View all players");
+            String get = myScnr.nextLine();
+
+            switch (get){
+                case "A":
+                    break;
+                case "B":
+                    break;
+                case "C":
+                    break;
+                case "D":
+                    break;
+                case "V":
+                    System.out.println("hi");
+            }
         }
+//        for(int i = 0 ;i< playerList.size(); i++) {
+//            System.out.println("Player Name : " + playerList.get(i).getPlayerName());
+//            System.out.println("Player Age : " + playerList.get(i).getPlayerAge());
+//            System.out.println("Player Type : " + playerList.get(i).getPlayerType());
+//            System.out.println("Player Statistics : " + playerList.get(i).getPlayerStatistics());
+//        }
     }
 
 
